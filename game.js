45e28cdaafbd4b1e9d98de6a8f78d6f6d8e26d43
Andrea Maahs <96502772+MaevynMade@users.noreply.github.com>
2021-12-22 08:13:42 -0800

    testing

---
+++ game.js
@@ -0,0 +1,35 @@
+class GameE{
+    constructor() {
+        this.canvas = document.getElementById("game");
+        this.context = this.canvas.getContext("2d");
+        this.spriteImage = new Image();
+        this.spriteImage.src = "flower.png";
+        const game = this;
+        this.spriteImage.onload = function(event){
+            const options = {
+                context: game.context, 
+                width: this.width,
+                height: this.height,
+                image: this
+            }
+            game.sprite = new Sprite (options);
+            game.sprite.render();
+        }
+    }
+}
+
+class Sprite{
+    constructor(options){
+        this.context = options.context;
+        this.width = options.width;
+        this.height = options.height;
+        this.image = options.image;
+    }
+    render(){
+        this.context.drawImage(
+            this.image,
+            200,
+            100
+        )
+    }
+}
